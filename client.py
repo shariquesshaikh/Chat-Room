9f38493f569d8fd7cbc4f9839a9c3751071059b5
sk-sharique <sharique.shaikh.0123@gmail.com>
2021-06-05 22:23:01 +0530

    admin and client permission added

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import threading
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
+n = len(nickname)
+
 if nickname == 'admin':
     password = input("Enter password for admin: ")
 
@@ -16,6 +18,7 @@ stop = False
 # Listening to Server and Sending Nickname
 def receive():
     while True:
+        global stop
         if stop:
             break
 
@@ -31,6 +34,9 @@ def receive():
                     if client.recv(1024).decode('ascii') == 'REFUSED':
                         print("Invalid Credentials. Retry... ")
                         stop = True
+                elif next_msg == 'BAN':
+                    print("Connection refused because you are banned from the chat room!")
+                    stop = True
             else:
                 print(message)
         except:
@@ -44,8 +50,19 @@ def write():
     while True:
         if stop:
             break
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ascii'))
+        message = f'{nickname}: {input("")}'
+        # global n
+        offset = n + 2
+        if message[offset:].startswith('/'):
+            if nickname == 'admin':
+                if message[offset:].startswith('/kick'):
+                    client.send(f'KICK {message[offset+6:]}'.encode('ascii'))
+                if message[offset:].startswith('/ban'):
+                    client.send(f'BAN {message[offset+5:]}'.encode('ascii'))
+            else:
+                print('Commands can only be executed by admin.')
+        else:
+            client.send(message.encode('ascii'))
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
--- server.py
+++ server.py
@@ -12,7 +12,7 @@ server.listen() #puts server to listening mode for incoming connections
 
 clients = [] #connected clients will be stored
 nicknames = [] #nicknames of connected clients
-
+banneduser = []
 # Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
@@ -22,27 +22,54 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)  # Broadcasting Messages
-            broadcast(message)
+            msg = message = client.recv(1024)  # Broadcasting Messages
+            ifadmin = (False, True) [nicknames[clients.index(client)] == 'admin']
+            
+            if msg.decode('ascii').startswith('KICK'):
+                if ifadmin:
+                    name_to_kick = msg.decode('ascii')[5:]
+                    kick_user(name_to_kick)
+                else:
+                    client.send('Admin role required to execute the command!').encode('ascii')
+            elif msg.decode('ascii').startswith('BAN') and ifadmin:
+                if ifadmin:
+                    name_to_ban = msg.decode('ascii')[4:]
+                    kick_user(name_to_ban)
+                    with open('bans.txt', 'a') as f:
+                        f.write(f'{name_to_ban}\n')
+                    print(f'{name_to_ban} was banned!')
+                else:
+                    client.send('Admin role required to execute the command!').encode('ascii')    
+            else:
+                broadcast(message)
         except:
-            index = clients.index(index) #index of a failed client
-            client.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))
-            nicknames.remove(nickname)
-            break
+            if client in clients:
+                index = clients.index(client) #index of a failed client
+                clients.remove(client)
+                client.close()
+                nickname = nicknames[index]
+                broadcast(f'{nickname} left the chat!'.encode('ascii'))
+                nicknames.remove(nickname)
+                break
 
 # Receiving / Listening Function
 def recieve():
     while True:
-        # Accept Connectio
+        # Accept Connection
         client, address = server.accept()
         print(f'Connected with {str(address)}')
 
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
 
+        with open('bans.txt', 'r') as f:
+            bans = f.readlines()
+        
+        if nickname + '\n' in bans:
+            client.send('BAN'.encode('ascii'))
+            client.close()
+            continue
+        
         if nickname == 'admin':
             client.send('PASSWORD'.encode('ascii'))
             password = client.recv(1024).decode('ascii')
@@ -63,6 +90,16 @@ def recieve():
         # Start Handling Thread For Client
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+def kick_user(name):
+    if name in nicknames:
+        name_index = nicknames.index(name)
+        client_to_kick = clients[name_index]
+        clients.remove(client_to_kick)
+        client_to_kick.send('You were kicked by an admin!'.encode('ascii'))
+        client_to_kick.close()
+        nicknames.remove(name)
+        broadcast(f'{name} was kicked by an admin!'.encode('ascii'))
 
 if __name__ == '__main__':
     print("Server is starting...[STARTED]")
